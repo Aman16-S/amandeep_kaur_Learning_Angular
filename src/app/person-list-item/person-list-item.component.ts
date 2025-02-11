8e7f3b6ad08b03f66a26af41a02f4cb00e8a0360
Amandeep Kaur <W0836054@myscc.ca>
2025-02-11 12:22:56 -0500

    seted version 0.2.0

---
+++ src/app/person-list-item/person-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-list-item',
+  imports: [],
+  templateUrl: './person-list-item.component.html',
+  styleUrl: './person-list-item.component.css'
+})
+export class PersonListItemComponent {
+
+}
