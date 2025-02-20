d0dcd56b796af032cc359503dd2a593e7d8195c2
Amandeep Kaur <W0836054@myscc.ca>
2025-02-20 12:31:41 -0500

    used dependency injection in PersonList component in order to add a new  service

--- src/app/person-list/person-list.component.ts
+++ src/app/person-list/person-list.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
-import { Person} from '../Shared/Models/person';
-import {NgClass, NgForOf} from "@angular/common";
+
+import {Component, OnInit} from '@angular/core';
+import {Person} from '../Shared/Models/person';
+import {NgForOf} from "@angular/common";
+import {PersonService} from '../services/person.service';
 import {PersonListItemComponent} from '../person-list-item/person-list-item.component';
 
 @Component({
-  selector: 'app-person-list',
+  selector: 'app-Person-list',
   standalone: true,
   imports: [
     NgForOf,
-    PersonListItemComponent,
-    NgClass,
-
+    PersonListItemComponent
   ],
-  templateUrl: './person-list.component.html',
-  styleUrls: ['./person-list.component.css']
+  templateUrl: './Person-list.component.html',
+  styleUrl: './Person-list.component.scss'
 })
-export class PersonListComponent {
-  // Placeholder values for the table
-  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'age', 'email', 'isAdmin'];
-  personList: Person[] = [];
+export class PersonListComponent implements OnInit {
+  //Placeholder values for the table
+  displayedColumns:string[]= ['id', 'firstName', 'lastName', 'age', 'isAdmin'];
+  PersonList: Person[] = [];
 
-  selectedPerson?: Person;
-
-  selectPerson(person: Person): void {
-    this.selectedPerson = person;
+  constructor (private PersonService: PersonService){
+    //this constructor is primarily used for dependency injection
   }
+
+
+  ngOnInit(){
+    //This lifecycle hook is a good place to fetch and init our data
+    this.PersonService.getPersons().subscribe({
+      next: (data: Person[]) => this.PersonList = data,
+      error:err => console.error("Error fetching Persons", err),
+      complete:() => console.log("Person data fetch complete!")
+    })
+
+  }
+  selectedPerson?: Person;
+  selectPerson(Person: Person): void {
+    this.selectedPerson = Person;
+  }
+
 }
