f07c3a98b872d145ef97536242d045e48fe6ecf7
Amandeep Kaur <W0836054@myscc.ca>
2025-02-20 18:27:01 -0500

    Defined and added routes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import {personListComponent} from "./app/person-list/person-list.component";
+import {personListComponent} from "./app/person-list/person-list.component";
+import {ModifypersonComponent} from "./app/modify-person/modify-person.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
-// Define routes (we will add components later)
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'', redirectTo: '/persons', pathMatch: 'full'}, //default route
+  { path: 'persons', component: personListComponent },
+  { path: 'persons/:id', component: personListComponent },
+  {path:'modify-person', component: ModifypersonComponent},
+  {path: '**', component:PageNotFoundComponent}//Wildcard route for a 404 page
+];
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)] // Setting up the router
-})
-  .catch(err => console.error(err));
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
