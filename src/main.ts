69c4e657617f75eb3a4eb6c0caccfe1d36e19c76
Amandeep Kaur <W0836054@myscc.ca>
2025-02-20 17:59:21 -0500

    Set up Angular Router in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Define routes (we will add components later)
+const routes: Routes = [];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)] // Setting up the router
+})
+  .catch(err => console.error(err));
